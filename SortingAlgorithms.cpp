0eff58bafc8b98600ae4848804985172545aa53c
Adan Alter <aalter1@ufl.edu>
2025-04-18 00:24:04 -0400

    Sorting Algorithms files added/downgraded cmake version require

---
+++ SortingAlgorithms.cpp
@@ -0,0 +1,53 @@
+//
+// Purpose: Class to implement merge sort, quick sort, and heap sort
+//
+
+#include "SortingAlgorithms.h"
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+
+/*
+
+Purpose: Initialize SortingAlgorithms Class
+Input: ;*/
+
+SortingAlgorithms::SortingAlgorithms() {
+    ;
+}
+
+/*
+
+Purpose: Implementation of the merge sort helper
+Input: ;*/
+
+void SortingAlgorithms::merge(vector<double>& arr, double left, double mid, double right) {
+    ;
+}
+
+/*
+
+Purpose: Implementation of the merge sort algorithm
+Input: ;*/
+
+void SortingAlgorithms::MergeSort(vector<double>& arr, double left, double right) {
+    ;
+}
+
+/*
+
+Purpose: Implementation of the quick sort algorithm
+Input: ;*/
+
+void SortingAlgorithms::quick() {
+    ;
+}
+/*
+
+Purpose: Implementation of the heap sort algorithm
+Input: ;*/
+
+void SortingAlgorithms::heap() {
+    ;
+}
---
+++ SortingAlgorithms.h
@@ -0,0 +1,27 @@
+//
+// Purpose: Class to implement merge sort, quick sort, and heap sort
+//
+
+#ifndef PROJECT_3_SORTALGORITHMS_H
+#define PROJECT_3_SORTALGORITHMS_H
+
+
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+class SortingAlgorithms {
+private:
+    void merge(vector<double>& arr, double left, double mid, double right);
+public:
+    SortingAlgorithms();
+    void MergeSort(vector<double>& arr, double left, double right);
+    void quick();
+    void heap();
+};
+
+#endif //PROJECT_3_SORTALGORITHMS_H
